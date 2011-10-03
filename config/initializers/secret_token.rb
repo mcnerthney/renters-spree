3b92a106df0da5098f6ed62a13464b3a08f397d5
Daniel McNerthney <dan.mcnerthney@softllc.com>
2011-10-03 09:50:44 -0700

    initial commit of spree store

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rentstore::Application.config.secret_token = 'f3dab8ca8d6681df72cb14fa8d8b63808a63c5e838dbeb5e7b1222720add9e2d5e1d76fe5442a4f8e6464a3310a71b956bb6aa392775fb94fe5aa757a9237ae1'
