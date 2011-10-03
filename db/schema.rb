7bb8410b4f32f9a13c3ab8676093f88d9261b51e
Daniel McNerthney <dan.mcnerthney@softllc.com>
2011-10-03 12:02:01 -0700

    seed store

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20111003165255) do
+ActiveRecord::Schema.define(:version => 20111003183505) do
 
   create_table "activators", :force => true do |t|
     t.string   "description"
